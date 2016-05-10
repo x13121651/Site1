4f44018fa196a31bf4f02957729afea247ac1b57
Ronan <ronan.lafford@student.ncirl.ie>
2016-05-11 00:43:53 +0100

    New deployment 11/05/16

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -32,16 +32,12 @@ export const MainLayout = ({content}) => (
                     </h1>
                     <img src="LOGO.png" className="ui medium fluid centered image"/>
 
-                    <div className="ui huge  basic blue button">Explore <i className="right arrow icon"></i>
+                    <div className="ui huge  basic blue button">View Portfolio <i className="right arrow icon"></i>
                     </div>
                 </div>
 
             </div>
 
-            <h4 class="ui horizontal divider header">
-                <i class="tag icon"></i>
-                Description
-            </h4>
 
                 <div className="ui equal width center aligned padded grid">
 
@@ -92,15 +88,64 @@ export const MainLayout = ({content}) => (
                         </div>
                     </div>
 
+                </div>
 
-                    <div class="ui inverted vertical footer segment form-page">
-                        <div class="ui container">
-                            Ronan Lafford 2016. All Rights Reserved
+            <div className="ui equal width center aligned padded grid">
+
+                <div className="olive column">
+                    <div className="ui raised centered huge link cards">
+                        <div className="card">
+                            <div className="image">
+                                <a href="http://www.blog.ronanlafford.me" class="ui medium image">
+                                    <img className="ui fluid image" src="blogpic.PNG"/>
+                                </a>
+                            </div>
+                            <div className="content">
+                                <div className="header">I've Caught The Bug!</div>
+                            </div>
+                            <div className="extra content">
+                                    <span className="center floated">
+                                         This is a blog of my learning.
+                                    </span>
+                                    <span>
+                                     <img className="left circular floated mini ui image" src="profile.jpg"/>
+                                    </span>
+                            </div>
                         </div>
                     </div>
-
-
                 </div>
+
+
+                <div className="purple column">
+                    <div className="ui centered huge link cards">
+                        <div className="card">
+                            <div className="image">
+                                <a href="http://www.dubloons.azurewebsites.net" class="ui medium image">
+                                    <img className="ui fluid image" src="dubpic.PNG"/>
+                                </a>
+                            </div>
+                            <div className="content">
+                                <div className="header">Dubloons</div>
+                            </div>
+                            <div className="extra content">
+                                    <span className="center floated">
+                                         This is a website for coin collectors.
+                                    </span>
+                                    <span>
+                                     <img className="left circular floated mini ui image" src="profile.jpg"/>
+                                    </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="ui inverted vertical footer segment form-page">
+                <div class="ui centered container">
+                    Ronan Lafford 2016. All Rights Reserved
+                </div>
+            </div>
 
             </body>
 
